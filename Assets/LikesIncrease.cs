4d3fd1661fac7b9eb2344a4fde93cbbc8e01cbad
ROBIN\Robin Klonk <robin.kocaurek@gmail.com>
2017-01-22 10:46:18 +0100

    changed balance, added images and sound

---
+++ Assets/LikesIncrease.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LikesIncrease : MonoBehaviour {
+	public int PostingValue = 1;
+
+	public void Increase () {
+		Game.Instance.Player.Follower.set = Game.Instance.Player.Follower.get + PostingValue * Game.Instance.Player.LikeMultiplier.get;
+	}
+}
